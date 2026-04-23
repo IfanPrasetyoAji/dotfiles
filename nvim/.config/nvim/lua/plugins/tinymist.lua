b9bd58a9a2e3ce709148ec6cdf5277059209a6b4
IfanPrasetyoAji <ifan.prasetyo.aji@gmail.com>
2026-04-23 23:51:33 +0700

    added typst plugin for nvim

---
+++ nvim/.config/nvim/lua/plugins/tinymist.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "tinymist",
+      },
+    },
+  },
+  {
+    "chomosuke/typst-preview.nvim",
+    lazy = false, -- or ft = 'typst'
+    version = "1.*",
+    opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+  },
+}
