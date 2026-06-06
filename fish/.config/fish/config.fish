69ca7d08c5922e4c9a1519bbaf393adc589b2f04
IfanPrasetyoAji <ifan.prasetyo.aji@gmail.com>
2026-06-06 18:52:48 +0700

    set nvim as fish's default editor, added zoxide

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,17 @@
 set -U fish_greeting ""
+set -Ux EDITOR nvim
+
+zoxide init fish | source
 
 function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	command yazi $argv --cwd-file="$tmp"
-	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
-		builtin cd -- "$cwd"
-	end
-	command rm -f -- "$tmp"
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+        builtin cd -- "$cwd"
+    end
+    command rm -f -- "$tmp"
 end
 
 if status is-interactive
-fastfetch
+    fastfetch
 end
